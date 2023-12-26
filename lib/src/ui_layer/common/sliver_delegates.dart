add6b19da5affc80fdbc1a603de994c769185437
Yogesh <yogesh.singh@appventurez.com>
2023-12-26 20:55:27 +0530

    optimization - in discover tab flow

---
+++ lib/src/ui_layer/common/sliver_delegates.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  SliverAppBarDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      color: Colors.white,
+      child: _tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(SliverAppBarDelegate oldDelegate) {
+    return false;
+  }
+}
